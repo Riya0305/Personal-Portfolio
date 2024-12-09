1e0d260625d11dddb6927494ef47879610675364
Riya Jain <riyajain0520@gmail.com>
2024-12-08 20:19:56 -0800

    final done

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 
-
 document.getElementById("contact-form").addEventListener("submit", function (event) {
     event.preventDefault(); // Prevent form submission
     const name = document.getElementById("name").value;
